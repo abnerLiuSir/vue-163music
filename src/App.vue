1e4488aa454914fab170fc1fea2d46bf27b06b08
abnerLiu <abnerliusir@163.com>
2020-10-08 20:33:27 +0800

    basic

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  mounted(){
+    axios.get('song/url?id=33894312&proxy=http://192.168.1.6:8080')
+      .then((data) => {
+        console.log(data);
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
